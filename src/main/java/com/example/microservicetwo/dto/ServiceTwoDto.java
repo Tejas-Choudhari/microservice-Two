643180b8533245603837ce5a52c70dd062339700
installuser <Tejas-hub-ttc>
2023-11-24 17:20:57 +0530

    added dto and updated format

---
+++ src/main/java/com/example/microservicetwo/dto/ServiceTwoDto.java
@@ -0,0 +1,26 @@
+package com.example.microservicetwo.dto;
+
+
+import lombok.Data;
+
+@Data
+public class ServiceTwoDto {
+
+    private String serviceName="microservice-two";
+    private long id;
+
+    //extra fields
+    private String requestTime;
+    private String responseTime;
+    private int StatusCode;
+    private String timeTaken;
+    private String requestURI;
+    private String requestMethod;
+    private String requestHeaderName;
+    private String contentType;
+    private String queryParam;
+    private String requestID;
+    private String hostName;
+    private String response;
+    private String errorTrace;
+}
